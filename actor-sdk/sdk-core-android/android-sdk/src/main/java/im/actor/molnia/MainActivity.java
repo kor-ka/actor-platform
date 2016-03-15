83329df576697bb4e8b0ecef1ed6f01126aba62d
kor_ka <badgrib@gmmail.com>
2016-03-15 15:08:58 +0300

    add search

--- actor-sdk/sdk-core-android/android-sdk/src/main/java/im/actor/molnia/MainActivity.java
+++ actor-sdk/sdk-core-android/android-sdk/src/main/java/im/actor/molnia/MainActivity.java
@@ -1,16 +1,30 @@
 package im.actor.molnia;
 
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.text.InputType;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import im.actor.core.entity.Dialog;
 import im.actor.core.entity.Peer;
+import im.actor.core.viewmodel.CommandCallback;
+import im.actor.core.viewmodel.UserVM;
 import im.actor.runtime.mvvm.Value;
 import im.actor.runtime.mvvm.ValueDoubleChangedListener;
+import im.actor.sdk.R;
 import im.actor.sdk.core.controllers.BaseFragmentActivity;
 import im.actor.sdk.core.controllers.dialogs.DialogsFragment;
 import im.actor.sdk.core.controllers.messages.ChatActivity;
+import im.actor.sdk.util.Screen;
 
 import static im.actor.sdk.util.ActorSDKMessenger.messenger;
 
@@ -70,5 +84,83 @@ public class MainActivity extends BaseFragmentActivity {
         if (longPeer != -1) {
             openDialog(Peer.fromUniqueId(longPeer));
         }
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        menu.clear();
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.add) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this, R.style.AlertDialogStyle);
+            String title = "Find contact";
+            builder.setTitle(title);
+
+            LinearLayout ll = new LinearLayout(this);
+            ll.setPadding(Screen.dp(20), 0, Screen.dp(20), 0);
+
+            final EditText input = new EditText(this);
+
+//            input.setTextColor(Color.BLACK);
+            ll.addView(input, LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+            builder.setView(ll);
+
+            builder.setPositiveButton(getString(R.string.dialog_ok), new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    execute(messenger().findUsers(input.getText().toString()), R.string.progress_common, new CommandCallback<UserVM[]>() {
+                        @Override
+                        public void onResult(UserVM[] res) {
+                            if (res.length > 0) {
+                                openDialog(Peer.user(res[0].getId()));
+                            } else {
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        Toast.makeText(MainActivity.this, "nothing found", Toast.LENGTH_LONG).show();
+                                    }
+                                });
+                            }
+                        }
+
+                        @Override
+                        public void onError(Exception e) {
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    Toast.makeText(MainActivity.this, "nothing found", Toast.LENGTH_LONG).show();
+                                }
+                            });
+                        }
+                    });
+                    ;
+                }
+            });
+            builder.setNegativeButton(R.string.dialog_cancel, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.cancel();
+                }
+            });
+
+
+            AlertDialog ad = builder.create();
+            ad.setOnShowListener(new DialogInterface.OnShowListener() {
+                @Override
+                public void onShow(DialogInterface dialog) {
+                    InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+                    input.requestFocus();
+                    inputMethodManager.showSoftInput(input, 0);
+                }
+            });
+            ad.show();
+            return true;
+        }
+
+        return false;
     }
 }
